cf1f4c2f1f97ede487d8e7bcc45b21f1810d2c2f
rose <rose@hello>
2014-04-20 11:19:17 +0800

    ParentFrame complete

---
+++ src/ui/ParentFrame.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javax.swing.JFrame;
+
+public class ParentFrame extends JFrame {
+	public ParentFrame() {
+		// TODO Auto-generated constructor stub
+		setSize(1500, 900);
+		setVisible(true);
+		setLayout(null);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+}
