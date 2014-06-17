243367edad4abbb928c40649c64aa032768c3931
Rose <desertblackeagle@gmail.com>
2014-06-17 11:49:53 +0800

    bat執行檔

---
+++ bat/client2.bat
@@ -0,0 +1,1 @@
+java -jar Client.jar 62cb1b250c6728e48adf5718abbbcd958d650d9ba9bde338527066d20f133b4d48837c72cfa641982136279050ca79fea6f4 fc9ccc5a6fbb820d142220ed0f97ffbce27e91eb4041cefeb738391bdb2d3b1f5357227f3322cb16c4dcc6dd151adac94adc bot2 https://sqa.swim-fish.info/media/noImageAvailable300.png
